e79a55db1696b2b83dde586bd41b671e17ad1f9d
Feriansyah <devferiansyah@gmail.com>
2023-07-22 23:03:13 +0800

    feat: slicing homepage

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
+await import('./src/env.mjs')
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -15,8 +15,11 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  images: {
+    domains: ['dummyimage.com'],
   },
   typescript: {
     ignoreBuildErrors: true,
@@ -25,6 +28,6 @@ const config = {
     ignoreDuringBuilds: true,
   },
   swcMinify: true,
-};
+}
 
-export default config;
+export default config
